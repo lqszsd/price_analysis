bf228750845758322eaca1745c8681c84cbb84c0
lq <1658616397@qq.com>
2022-02-26 17:43:38 +0800

    提交前端

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 	"strconv"
 )
 
-func main(){
+func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		resBody:=request.Body
+		resBody := request.Body
 
 		param := &struct {
-			Symbol string `json:"symbol"`
+			Symbol    string `json:"symbol"`
 			StartDate string `json:"start_date"`
-			EndDate string `json:"end_date"`
+			EndDate   string `json:"end_date"`
 		}{}
 		// 通过json解析器解析参数
 		json.NewDecoder(resBody).Decode(param)
@@ -24,9 +24,9 @@ func main(){
 		writer.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
 		writer.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 		writer.Header().Set("content-type", "application/json")             //返回数据格式是json
-		res,_:=http.Get("http://127.0.0.1:8080/api/bond_zh_hs_cov_min?symbol="+param.Symbol+"&start_date="+param.StartDate+"&end_date="+param.EndDate+"&adjust=hfq")
-		fmt.Println("http://127.0.0.1:8080/api/bond_zh_hs_cov_min?symbol="+param.Symbol+"&start_date="+param.StartDate+"&end_date="+param.EndDate+"&adjust=hfq")
-		by,_:=ioutil.ReadAll(res.Body)
+		res, _ := http.Get("http://127.0.0.1:8080/api/public/bond_zh_hs_cov_min?symbol=" + param.Symbol + "&start_date=" + param.StartDate + "&end_date=" + param.EndDate + "&adjust=hfq")
+		fmt.Println("http://127.0.0.1:8080/api/public/bond_zh_hs_cov_min?symbol=" + param.Symbol + "&start_date=" + param.StartDate + "&end_date=" + param.EndDate + "&adjust=hfq")
+		by, _ := ioutil.ReadAll(res.Body)
 		writer.Write(by)
 	})
 
@@ -37,31 +37,30 @@ func main(){
 		writer.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
 		writer.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 		writer.Header().Set("content-type", "application/json")             //返回数据格式是json
-		list:=service.Filter_expensive_list()
-		data,_:=json.Marshal(list)
+		list := service.Filter_expensive_list()
+		data, _ := json.Marshal(list)
 		writer.Write(data)
 	})
 
 	http.HandleFunc("/api/recommend", func(writer http.ResponseWriter, request *http.Request) {
-		code:=request.FormValue("code")
-		price:=request.FormValue("price")
+		code := request.FormValue("code")
+		price := request.FormValue("price")
 		// 通过json解析器解析参数
 
 		writer.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
 		writer.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 		writer.Header().Set("content-type", "application/json")             //返回数据格式是json
-		money,_:=strconv.ParseFloat(price,64)
-		list:=service.GetMoneyRecommend(code,money)
-		data,_:=json.Marshal(list)
+		money, _ := strconv.ParseFloat(price, 64)
+		list := service.GetMoneyRecommend(code, money)
+		data, _ := json.Marshal(list)
 		writer.Write(data)
 	})
-	err:=http.ListenAndServe("127.0.0.1:9090",nil)
+	err := http.ListenAndServe("127.0.0.1:9090", nil)
 	fmt.Println(err)
-	for{
+	for {
 		fmt.Println(err)
 	}
 
 }
-
 
 //https://a1.m1907.cn/api/v/?z=f132c044a68b5a642e89f9157518addb&jx=https://m.iqiyi.com/v_26ex87ts5gc.html&s1ig=11401&g=
